20bf1502ef34532527aaeb4f3878277f22582009
Damian Duszny <damian.duszny@gmail.com>
2024-04-17 23:28:56 +0200

    added recipes types

--- app/Models/recipes.php
+++ app/Models/recipes.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class recipes extends Model
 {
@@ -15,4 +17,8 @@ class recipes extends Model
         'portion_for_how_many',
         'complexity',
     ];
+
+    public function type():BelongsTo {
+        return $this->belongsTo(recipes_types::class);
+    }
 }
