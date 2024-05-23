9b70d20770b2c375a41bd3c9f2997d0d67802501
Damian Duszny <damian.duszny@gmail.com>
2024-05-23 18:12:43 +0200

    docs and small cosmetic changes

--- app/Http/Controllers/Api/ShoppingListController.php
+++ app/Http/Controllers/Api/ShoppingListController.php
@@ -17,17 +17,21 @@ class ShoppingListController extends Controller
     }
 
     /**
-     * @param recipes[] $recipes
-     * @return void
+     * Creating needed products for given recipes. Recipes must be connected to current logged user.
+     * Recipes are loaded with dependency injection using post array variable 'recipes_id' {@see \App\Providers\AppServiceProvider::register}.
+     * Endpoint can check user pantry for this operation, if is needed just use post variable 'check_pantry' [0,1]
+     *
+     * @param RecipesList $recipesList auto added using post array variable recipes_ids
+     * @param Request $request
+     * @return array
      */
     public function createShoppingListFromRecipes(RecipesList $recipesList, Request $request): array {
         /** @var User $user */
         $user = auth('sanctum')->user();
-        $neededProducts = (new ShoppingList($user))->transformRecipesListToShoppingList(
+        return (new ShoppingList($user))->transformRecipesListToShoppingList(
             $recipesList,
             (bool)$request->post('check_pantry'),
             array_count_values($request->post('recipes_ids'))
         );
-        return $neededProducts;
     }
 }
--- app/Services/ShoppingList.php
+++ app/Services/ShoppingList.php
@@ -11,11 +11,36 @@ class ShoppingList
 
     private array $userPantryProductsChangable;
 
-    private array $userPantryProducts;
+    private readonly array $userPantryProducts;
 
-    public function __construct(private User $user){}
+    public function __construct(private readonly User $user)
+    {
+    }
 
-    public function transformRecipesListToShoppingList(RecipesList $recipesList, bool $checkPantry = true, array $multipleRecipes = [])
+    /**
+     * Returns array:
+     * {
+     *  products_id: {
+     *      recipes_id: {
+     *          "products_id": products_id - id from products table,
+     *          "main_amount_needed": int - recipe needed product amount - pantry products (if argument checkPantry is set to true),
+     *          "recipes_id": int - recipes_id from users_recipes table,
+     *          "substitute_product_id": int - id from products table,
+     *          "main_amount_in_pantry": int - main recipe product amount in pantry,
+     *          "substitute_amount_in_pantry": int - substitute recipe product amount in pantry,
+     *          "substitute_amount_needed": int - recipe substitute for main product needed amount - pantry products (if argument checkPantry is set to true)
+     *      }
+     *  }
+     * }
+     *
+     * @param RecipesList $recipesList
+     * @param bool $checkPantry if is set to true, user pantry products amount will be checked and subtracted from needed
+     * product amount for recipe. Main products have priority over substitute products.
+     * @param array $multipleRecipes
+     * @return array
+     */
+
+    public function transformRecipesListToShoppingList(RecipesList $recipesList, bool $checkPantry = true, array $multipleRecipes = []): array
     {
         if ($checkPantry) {
             $this->setUserPantryProducts(
@@ -42,7 +67,7 @@ class ShoppingList
         }
 
         foreach ($neededProducts as $recipesProducts) {
-            foreach($recipesProducts as $product) {
+            foreach ($recipesProducts as $product) {
                 $substitute = $this->findSubstitutesForProductInRecipesList($recipesList, $product);
                 if (empty($substitute)) {
                     continue;
@@ -59,7 +84,7 @@ class ShoppingList
         return $neededProducts;
     }
 
-    private function findSubstitutesForProductInRecipesList(RecipesList $recipesList, array $mainProduct)
+    private function findSubstitutesForProductInRecipesList(RecipesList $recipesList, array $mainProduct): array
     {
         foreach ($recipesList->getProductsFromRecipes()['substitute_products'] as $product) {
             if ($product['substitute_for'] === $mainProduct['products_id'] && $product['recipes_id'] === $mainProduct['recipes_id']) {
@@ -69,7 +94,13 @@ class ShoppingList
         return [];
     }
 
-    private function setUserPantryProducts($productsIds)
+    /**
+     * Sets two properties {@see userPantryProducts} and {@see userPantryProductsChangable}.
+     *
+     * @param $productsIds
+     * @return void
+     */
+    private function setUserPantryProducts($productsIds): void
     {
         $this->userPantryProducts = $this->user
             ->products()
@@ -85,7 +116,15 @@ class ShoppingList
         $this->userPantryProductsChangable = $this->userPantryProducts;
     }
 
-    private function getNeededAmountForProduct(array $product, bool $checkPantry = true, bool $subtractAmountFromPantry = true)
+    /**
+     * Calculate needed amount for product. If checkPantry is set to true will subtract amount from {@see userPantryProductsChangable}
+     *
+     * @param array $product
+     * @param bool $checkPantry
+     * @param bool $subtractAmountFromPantry
+     * @return int
+     */
+    private function getNeededAmountForProduct(array $product, bool $checkPantry = true, bool $subtractAmountFromPantry = true): int
     {
         if (!$checkPantry) {
             return $product['amount'];
